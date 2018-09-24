9f21efd9b880b2d69d072c5b7bfe18c9aebfd485
NOZGART\sergey <gocodegaming@gmail.com>
2018-09-24 18:46:34 +0300

    не сломайся пжлста

--- Source/TestingGrounds/Private/ChooseNextWaypoint.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "ChooseNextWaypoint.h"
-
-EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
-{
-	UE_LOG(LogTemp, Warning, TEXT("C++ HELLO"));
-	return EBTNodeResult::Succeeded;
-}
